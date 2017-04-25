bcc6a2f1d19dd9a9888f63d9ee07b34bcac57276
Joe Brew <joebrew@gmail.com>
2017-04-25 23:08:44 +0200

    major changes

---
+++ garmin.R
@@ -0,0 +1,398 @@
+# library(cycleRtools)
+library(xml2)
+library(XML)
+library(tidyverse)
+
+# Define a directory with the txc files
+tcx_dir <- '/home/joebrew/Dropbox/Aplicaciones/tapiriik/'
+files <- dir(tcx_dir)
+# files <- files[length(files)]
+original_files <- files
+files <- file.path(tcx_dir, files)
+file <- files
+
+# Define function for reading tcx files
+read_tcx <- function(file){
+  message(paste0('Reading file ', 
+                 file))
+  
+  x <- xmlToList(file)
+  y <- plyr::ldply(x, data.frame)
+  # Keep only the first row
+  y <- y[1,]
+  # Keep lap information elsewhere
+  lap_info <- y[,grepl('Activity.Lap', names(y)) & !grepl('Trackpoint', names(y))]
+  # Keep only those columns which contain tracking
+  y <- y[,grepl('Activity.Lap.Track.Trackpoint', names(y))]
+
+  # Define helper for cleaning_up
+  clean_up <- function(df){
+    df <- t(df)
+    df <- data.frame(df)
+    df$indicator <- row.names(df)
+    df$value <- df$X1
+    row.names(df) <- NULL
+    df$X1 <- NULL
+    # Remove the useless parts of indicator
+    df$indicator <- gsub('Activity.Lap.Track.Trackpoint.',
+                        '',
+                        df$indicator)
+    
+    # Extract the number from indicator
+    numbers <- regmatches(df$indicator, 
+                          gregexpr("[[:digit:]]+", df$indicator))
+    numbers <- lapply(numbers, 
+                      function(x){
+                        out <- unlist(x)
+                        if(length(out) == 0){
+                          out <- NA
+                        }
+                        return(out)
+                      })
+    numbers <- unlist(numbers)
+    df$number <- numbers
+    df$number <- as.numeric(as.character(df$number))
+    df$number[is.na(df$number)] <- 0
+    # Increase number by 1
+    df$number <- df$number + 1
+    
+    # Remove the number from indicator
+    df$indicator <- gsub('[[:digit:]]+', 
+                        '',
+                        df$indicator)
+    df$indicator <- as.character(df$indicator)
+    df$value <- as.character(df$value)
+    # remove trailing periods
+    df$indicator <-
+      gsub("\\.(?=\\.*$)", "", df$indicator, perl=TRUE)
+    return(df)
+  }
+  
+  # Clean up lap info
+  lap_info <- clean_up(lap_info)
+  # Clean up names
+  lap_info <-
+    lap_info %>%
+    mutate(indicator = gsub('Activity.Lap.',
+                            '',
+                            indicator))
+  # Keep only relevant indicators
+  lap_info <-
+    lap_info %>%
+    filter(indicator == 'DistanceMeters')
+  # Get cumulative distance
+  # lap_info$cumulative_seconds <- cumsum(lap_info$value)
+  lap_info$distance <- cumsum(lap_info$value)
+  
+  # Clean up trackpoint info
+  z <- clean_up(y)
+  
+  # Group by number and get values
+  out <- z %>%
+    dplyr::group_by(number) %>%
+    summarise(altitude = dplyr::first(value[indicator == 'AltitudeMeters']),
+              distance = dplyr::first(value[indicator == 'DistanceMeters']),
+              cadence = dplyr::first(value[indicator == 'Extensions.TPX.RunCadence']),
+              speed = dplyr::first(value[indicator == 'Extensions.TPX.Speed']),
+              latitude = dplyr::first(value[indicator == 'Position.LatitudeDegrees']),
+              longitude = dplyr::first(value[indicator == 'Position.LongitudeDegrees']),
+              time = dplyr::first(value[indicator == 'Time'])) %>%
+    mutate(altitude = as.numeric(altitude),
+           distance = as.numeric(distance),
+           cadence = as.numeric(cadence),
+           speed = as.numeric(speed),
+           latitude = as.numeric(latitude),
+           longitude = as.numeric(longitude))
+  # Bring in lap info
+  out$lap <- 0
+  for (i in 1:nrow(lap_info)){
+    if(i == 1){
+      lower <- -1
+    } else {
+      lower <- lap_info$distance[i-1]
+    }
+    upper <- lap_info$distance[i]
+    
+    out$lap[out$distance <= upper &
+              out$distance > lower] <-
+      lap_info$number[i]
+  }
+  
+  # Clarify that number is seconds
+  out <-
+    out %>%
+    rename(seconds = number)
+  return(out)
+}
+
+results_list <- list()
+for (i in 1:length(files)){
+  message(i)
+  activity_name <- gsub('.tcx', '',
+                        original_files[i])
+  file_name <- paste0(activity_name,
+                      '.RData')
+  date <- as.Date(substr(activity_name, 1, 10))
+  if(file_name %in% dir('rdatas')){
+    load(paste0('rdatas/', file_name))
+  } else {
+    this_run <- read_tcx(files[i])
+    save(this_run,
+         file = paste0('rdatas/', file_name))
+  }
+  this_run$date <- date
+  this_run$activity_name <- activity_name
+  results_list[[i]] <- this_run
+}
+results <- bind_rows(results_list)
+
+
+
+# # ###############################################
+# # activity_dir <- '/media/joebrew/GARMIN/GARMIN/ACTIVITY/'
+# # plugged_in <- dir.exists(activity_dir)
+# # 
+# # # If connected to device, copy the files to local hard drive
+# # if(plugged_in){
+# #   files <- dir(activity_dir)
+# #   files <- file.path(activity_dir, files)
+# #   
+# #   # Copy to /data
+# #   if(plugged_in){
+# #     for(i in 1:length(files)){
+# #       this_file <- files[i]
+# #       file.copy(from=  this_file,
+# #                 to = paste0('data', gsub(activity_dir, '', this_file)))
+# #       message(this_file)
+# #     }
+# #   }
+# # }
+# # 
+# # # Define local files
+# # local_files <- dir('data')
+# # 
+# # # Read in local files
+# # results <- list()
+# # for (i in 1:length(local_files)){
+# #   try({
+# #     x <- cycleRtools::read_fit(paste0('data/', local_files[i]), 
+# #                                format = TRUE)
+# #     x$activity_number <- i
+# #     x$file <- local_files[i]
+# #     results[[i]] <- x
+# #   })
+# # }
+# # 
+# # # Bind all together
+# # results <- bind_rows(results)
+# 
+
+# Define function for converting meters per second
+# to minutes per kilometer
+mps_to_pace <- function(x){
+  1000 / x / 60
+}
+results$pace <- mps_to_pace(results$speed)
+results$pace[is.infinite(results$pace)] <- NA
+results$pace[results$pace > 100] <- NA
+
+# Create pace curve
+pc <- 
+  results %>%
+  arrange(activity_name, desc(speed)) %>%
+  mutate(dummy = 1) %>%
+  group_by(activity_name) %>%
+  mutate(y = cumsum(dummy)) %>%
+  mutate(p = y / max(y) * 100)
+
+activity_names <- sort(unique(pc$activity_name))
+ggplot(data = pc %>%
+         filter(activity_name %in% activity_names[10:25]),
+       aes(x = p,
+           y = speed)) +
+  geom_line() +
+  facet_wrap(~activity_name)
+
+# # Define function converting kph to pace per k
+# kph_to_pace <- function(x){
+#   60 / x
+# }
+# results$pace <- kph_to_pace(results$speed.kmh)
+# results$pace[is.infinite(results$pace)] <- NA
+# 
+# 
+# Define function for being in marathon pace zone
+results <-
+  results %>%
+  mutate(marathon_pace = pace <= 4.6) #4:36
+results$marathon_pace[is.na(results$marathon_pace) |
+                        is.infinite(results$marathon_pace)] <-
+  FALSE
+# # Get a date
+# results <- results %>%
+#   mutate(date = as.Date(timestamp))
+# 
+# Get a diff on the difference
+results <- results %>%
+  group_by(activity_name) %>%
+  mutate(diff_distance = distance - dplyr::lag(distance, default = 0))
+# 
+# Get how much was at marathon pace for each day
+by_day <- results %>%
+  group_by(date, marathon_pace) %>%
+  summarise(distance = sum(diff_distance)) %>%
+  ungroup
+# 
+library(ggplot2)
+ggplot(data = by_day,
+       aes(x = date,
+           y = distance,
+           group = marathon_pace,
+           fill = marathon_pace)) +
+  geom_bar(stat = 'identity', position = 'stack')
+# 
+
+# Get by week
+by_week <- 
+  results %>%
+  mutate(week = format(date, '%U')) %>%
+  group_by(week, marathon_pace) %>%
+  summarise(distance = sum(diff_distance),
+            runs = length(unique(activity_name))) %>%
+  ungroup %>%
+  mutate(week = as.numeric(week) - 11) %>%
+  filter(week != 0)
+ggplot(data = by_week,
+       aes(x = week,
+           y = distance,
+           group = marathon_pace,
+           fill = marathon_pace)) +
+  geom_bar(stat = 'identity', position = 'stack')
+
+library(cism)
+library(sp)
+library(ggthemes)
+library(ggmap)
+plot(man2)
+# 
+marathon_pace <- 14.0625
+# 
+
+# Create a pace curve
+pc <- 
+
+# For now, keep only manhica
+# removing calanga, bilene, komati, etc.
+results <-
+  results %>%
+  filter(round(longitude) == 33,
+         (date < '2017-04-12' | date > '2017-04-13'),
+         date != '2017-03-27')
+
+# x <- get_map(location = results %>%
+#                ungroup %>%
+#                summarise(longitude = mean(longitude, na.rm = TRUE),
+#                          latitude = mean(latitude, na.rm = TRUE)) %>%
+#                unlist(),
+#             zoom = 12,
+#             maptype = 'toner',
+#             source = 'stamen',
+#             color = 'bw')
+# y <- get_map(location = results %>%
+#                 ungroup %>%
+#                 summarise(longitude = mean(longitude, na.rm = TRUE),
+#                           latitude = mean(latitude, na.rm = TRUE)) %>%
+#                 unlist(),
+#               zoom = 14,
+#               maptype = 'toner',
+#               source = 'stamen',
+#               color = 'bw')
+# 
+# ggmap(x,
+#       darken = 0.85) +
+#   geom_path(data = results,
+#             aes(x = longitude,
+#                 y = latitude,
+#                 group = activity_name),
+#             alpha = 0.1,
+#             color = 'darkorange',
+#             size = 0.4) +
+#   coord_map() +
+#   theme_map()
+
+# library(leaflet)
+# leaflet() %>%
+#   # addProviderTiles('Esri.WorldImagery') %>%
+#   # addProviderTiles('OpenStreetMap.BlackAndWhite') %>%
+#   addProviderTiles('Stamen.Watercolor') %>%
+#   addPolylines(data = results, lng = ~longitude,
+#                lat = ~latitude, group = ~activity_name)
+# 
+# # Calanga
+# leaflet() %>%
+#   addProviderTiles('Esri.WorldImagery') %>%
+#   # addProviderTiles('OpenStreetMap.BlackAndWhite') %>%
+#   # addProviderTiles('Stamen.Watercolor') %>%
+#   addPolylines(data = results %>%
+#                  filter(date == '2017-03-27') %>%
+#                  filter(activity_name == min(activity_name)),
+#                lng = ~longitude,
+#                latitude = ~latitude, group = ~activity_name)
+# ggplot() +
+#   geom_path(data = results,
+#             aes(x = longitude,
+#                 y = latitude,
+#                 group = activity_name),
+#             alpha = 0.3,
+#             color = 'darkred',
+#             size = 0.3) +
+#   coord_map() +
+#   theme_map()
+print(head(results))
+g <- ggplot(data = results,
+       aes(x = longitude,
+           y = latitude,
+           group = activity_name)) +
+  geom_path(#aes(color = altitude),
+            alpha = 0.6,
+            color = 'darkred',
+             size = 0.1) +
+  coord_map() +
+  theme_map() +
+  # scale_color_continuous_tableau('Red',
+  #                                name = 'Elevation') +
+  theme(legend.key.size = unit(2, 'mm')) 
+print(g)
+
+# 
+# ggplot(data = results %>%
+#          group_by(activity_name) %>%
+#          mutate(totalk = sum(distance.km)) %>%
+#          mutate(date = first(date)) %>%
+#          ungroup %>%
+#          group_by(date) %>%
+#          mutate(activity = 1 + activity_name - min(activity_name)) %>%
+#          ungroup %>%
+#          mutate(activity = letters[activity]) %>%
+#          filter(totalk > 0) %>%
+#          mutate(date_activity = paste0(date, ': ', activity)),
+#        aes(x = timer.min,
+#            y = speed.kmh)) +
+#   geom_area(alpha = 0.6,
+#             fill = 'darkorange') +
+#   geom_hline(yintercept = marathon_pace,
+#              lty = 2) +
+#   facet_wrap(~date_activity) +
+#   theme_bw()
+# 
+# x <- intervals %>%
+#   group_by(marathon_pace) %>%
+#   summarise(time = sum(delta.t) / 60,
+#             dist = sum(diff_distance))
+# 
+# ggplot( data = intervals,
+#         aes(x = timer.min,
+#             y = marathon_pace)) +
+#   geom_jitter(alpha = 0.3)
+
+
--- plan.R
+++ plan.R
@@ -7,7 +7,10 @@ dates <- seq(as.Date('2017-03-13'),
 day_number <- 1:length(dates)
 
 # Divide into weeks
-weeks <- (day_number %/% 7) + 1
+weeks <- ((day_number - 1) %/% 7) + 1
+
+# Get number of weeks
+max(weeks)
 
 # Create a dataframe
 df <- data_frame(date = dates,
@@ -16,45 +19,138 @@ df <- data_frame(date = dates,
 # Get day of week
 df$dow <- weekdays(df$date)
 
-# Idea: 3 workouts per week, with 1, 2 and 3 reps (respectively)
+# Idea: 3 workouts per week, with 2, 2 and 4 reps (respectively)
 # of an identical distance at race pace.
 
 # Tuesday: 2
-# Thursday: 1
-# Saturday: 3
+# Thursday: 2
+# Saturday: 4
 
 # Progression should be non-linear (since it's easy to go from 1 to 2 k
 # harder to go from 8 to 9)
 # Progression is number of kilometers per rep
-progression <- seq(1, 500, length = nrow(df))^0.4
-progression <- round(progression)
-
+# progression <- seq(1, 40, length = nrow(df))^0.7
+# progression <- round(progression)
+progression <- c(rep(2, 14),
+                 rep(3, 7),
+                 rep(4, 7),
+                 rep(3, 7),
+                 rep(4, 7),
+                 rep(5, 7),
+                 rep(4, 7),
+                 rep(5, 7),
+                 rep(6, 7),
+                 rep(5, 7),
+                 rep(6, 7),
+                 rep(7, 7),
+                 rep(6, 7),
+                 rep(7, 7),
+                 rep(8, 7),
+                 rep(7, 7),
+                 rep(8, 7),
+                 rep(9,7),
+                 rep(8, 7),
+                 rep(9, 7), 
+                 rep(10, 7),
+                 rep(9, 7),
+                 rep(10, 7),
+                 rep(11, 7),
+                 rep(10, 7),
+                 rep(11, 7),
+                 rep(12, 7),
+                 # Begin taper
+                 rep(10, 21))
+                 
 df$km_per_rep <- progression
 
 # Plot the progression
 plot(df$date, df$km_per_rep, type = 'l')
 
+abline(v = Sys.Date(), 
+       col = 'red')
+
 # Get number of reps
 df$number_of_reps <-
   ifelse(df$dow == 'Tuesday', 2,
-         ifelse(df$dow == 'Thursday', 1,
-                ifelse(df$dow == 'Saturday', 3, 0)))
+         ifelse(df$dow == 'Thursday', 2,
+                ifelse(df$dow == 'Saturday', 4, 0)))
 
 # Get number of kilometers at race pace
 df$km_at_race_pace <- df$number_of_reps * df$km_per_rep
 
-# Remove the last day
-df <- df %>% filter(date < '2017-10-15')
+# Remove km per rep, if no reps that day
+df$km_per_rep[df$number_of_reps == 0] <- 0
+
+# Pick some time trial dates
+time_trial_dates <-
+  as.Date(c(#'2017-04-30', 
+            #'2017-05-30', 
+            '2017-06-18',
+            '2017-07-30',
+            '2017-08-27',
+            '2017-09-24'))
+
+# Spread data
+df$workout <- 
+  ifelse(df$dow == 'Sunday', 'rest',
+         ifelse(df$km_per_rep == 0, 
+         'easy + sprints',
+         paste0(df$number_of_reps, 'x',
+                df$km_per_rep, 'k')))
+
+# Update time trial dates
+df$workout <-
+  ifelse(df$date %in% time_trial_dates,
+         'time trial',
+         df$workout)
+
+
+wide <- df %>%
+  mutate(date = format(date, '%b %d')) %>%
+  group_by(week) %>%
+  summarise(dates = paste0(first(date), '-', last(date)),
+            Monday = workout[dow == 'Monday'],
+            Tuesday = workout[dow == 'Tuesday'],
+            Wednesday = workout[dow == 'Wednesday'],
+            Thursday = workout[dow == 'Thursday'],
+            Friday = workout[dow == 'Friday'],
+            Saturday = workout[dow == 'Saturday'],
+            Sunday = workout[dow == 'Sunday'])
+
 
 # Group by week and get total number of km at race pace per week
 by_week <- df %>%
   group_by(week) %>%
-  summarise(km_at_race_pace = sum(km_at_race_pace)) %>%
+  mutate(date_start = first(date)) %>%
+  filter(km_per_rep > 0) %>%
+  summarise(date_start = first(date_start),
+            km_at_race_pace = sum(km_at_race_pace),
+            km_per_rep = mean(km_per_rep),
+            tuesday = number_of_reps[dow == 'Tuesday'],
+            thursday = number_of_reps[dow == 'Thursday'],
+            saturday = number_of_reps[dow == 'Saturday']) %>%
   ungroup
 
 # Plot
 ggplot(data = by_week,
        aes(x = week,
-           y = km_at_race_pace)) +
+           y = km_per_rep)) +
   geom_line() +
-  geom_area(alpha = 0.3)
+  geom_area(alpha = 0.3) +
+  geom_vline(xintercept = df$week[df$date == Sys.Date()],
+             alpha = 0.5)
+
+ggplot(data = df %>%
+         dplyr::filter(dow %in% c('Tuesday', 'Thursday', 'Saturday')),
+       aes(x = date,
+           y = km_at_race_pace)) +
+  geom_point(alpha = 0.6) +
+  geom_line(alpha = 0.2) +
+  geom_vline(xintercept = as.numeric(Sys.Date()),
+             color = 'darkred',
+             alpha = 0.5) +
+  geom_hline(yintercept = c(seq(0, 40, 10), 42.195),
+             color = 'red', alpha = 0.2)
+
+
+write_csv(df, '~/Desktop/sub3.csv')
